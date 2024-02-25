865cdcafa73cd3219a37cfb941e4cf4e1416d38b
kai tran <ultra1524@gmail.com>
2024-02-25 14:21:48 -0800

    Initial Commit

---
+++ catApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  catApp
+//
+//  Created by Triet Tran on 2/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("HelloWORLD")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ catApp/catAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  catAppApp.swift
+//  catApp
+//
+//  Created by Triet Tran on 2/25/24.
+//
+
+import SwiftUI
+
+@main
+struct catAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
